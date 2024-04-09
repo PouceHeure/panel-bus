73fa6a8ac4ec6a78720c3601bfcc8880e24096e6
Hugo Pousseur <hugo.pousseur@hds.utc.fr>
2024-04-09 15:04:49 +0200

    add param in the link

--- js/load-bus.js
+++ js/load-bus.js
@@ -1,18 +1,30 @@
-const stationID = 31500 // guy denielou 
-const stationName = "Guy Denielou"
+// by default
+stationID = 31500 // guy denielou 
+stationName = "Waiting Connection"
+
 const intervalTimeRefresh = 20 * 1000 // ms
 const directionMetaNames = { // "ligne.number": "direction.id"  
     "1": { "1": "Hôpital", "2": "Gare" },
     "2": { "1": "Clairoix", "2": "Venette" },
     "3": { "1": "Belin", "2": "Gare" },
+    "4": { "1": "Gare", "2": "Venette" },
     "5": { "1": "Hôpital", "2": "Gare" },
     "6": { "1": "Venette", "2": "Gare" },
     "ARC Express": { "1": "Verberie", "2": "Gare" },
 };
 
+function getStationIDFromURL() {
+    const params = new URLSearchParams(window.location.search);
+    return params.get('stationID'); // "31500" si l'URL est index.html?stationID=31500
+}
 
 
 document.addEventListener('DOMContentLoaded', function() {
+    const newStationID = getStationIDFromURL();
+    console.log("test" + newStationID);
+    if(newStationID){
+        stationID = newStationID
+    }
     fetchAndDisplayBusSchedule();
     setInterval(fetchAndDisplayBusSchedule, intervalTimeRefresh);
  });
@@ -20,14 +32,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
  
  function fetchAndDisplayBusSchedule() {
-    fetch('https://api.oisemob.cityway.fr/media/api/v1/fr/Schedules/LogicalStop/31500/NextDeparture?realTime=true&lineId=&direction=&userId=TSI_OISEMOB')
+    fetch('https://api.oisemob.cityway.fr/media/api/v1/fr/Schedules/LogicalStop/'+stationID+'/NextDeparture?realTime=true&lineId=&direction=&userId=TSI_OISEMOB')
     .then(response => response.json())
     .then(data => {
+        if (data && data.length > 0 && data[0].lines && data[0].lines.length > 0) {
+            // Met à jour stationName avec le nom du premier arrêt trouvé
+            stationName = data[0].lines[0].stop.name;
+            document.getElementById('currentTime').textContent = `${new Date().toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'})} - ${stationName}`;
+        }
         displayBusSchedule(data);
     })
     .catch(error => console.error('Erreur lors de la récupération des données:', error));
-    
     document.getElementById('currentTime').textContent = `${new Date().toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'})} - ${stationName}`;
+    
 }
 
 const displayBusSchedule = (busData) => {
@@ -43,7 +60,7 @@ const displayBusSchedule = (busData) => {
                 const lineTitle = document.createElement('div');
                 lineTitle.classList.add('line-title');
                 lineTitle.style.backgroundColor = `#${line.line.color}`;
-                const info_direction = directionMetaNames[line.line.number][line.direction.id];
+                const info_direction = directionMetaNames[line.line.number]?.[line.direction.id] ?? line.direction.name;
                 lineTitle.innerHTML = `Ligne ${line.line.number} <span class='direction-title'>(${info_direction})</span>`;
                 lineContainer.appendChild(lineTitle);
 
@@ -64,7 +81,7 @@ const displayBusSchedule = (busData) => {
 
                     // Utiliser timeDifference pour les temps en dessous de 1 minute
                     if (time.realDateTime && diff < 1) {
-                        const seconds = time.timeDifference * 60; // Convertir timeDifference en secondes
+                        const seconds = time.timeDifference;
                         timeElement.textContent = `(< 1 min) ${seconds} sec`;
                     } else {
                         timeElement.textContent = `${Math.round(diff)} min`;
@@ -80,11 +97,3 @@ const displayBusSchedule = (busData) => {
         }
     });
 };
-
-
-fetch('https://api.oisemob.cityway.fr/media/api/v1/fr/Schedules/LogicalStop/'+stationID+'/NextDeparture?realTime=true&lineId=&direction=&userId=TSI_OISEMOB')
-.then(response => response.json())
-.then(data => {
-    displayBusSchedule(data);
-})
-.catch(error => console.error('Failed to download or exploit information from API', error));
